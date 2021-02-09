0c70e28433a2955d4b7ff1888ccfb6c7d30ebb9e
izumi1019 <khm1_frog@outlook.jp>
2021-02-09 04:05:26 +0000

    いいねまで

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,10 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '4cbfea8dbff9e4619df2401b5e827f6dccdfa7ef34864fd9688c95c876fa1560d82abffa2b5a20b4eb615783c5f406db756e572323ccf8d129e4e5d4e05f5f5f'
+
+
+
+
+
+
